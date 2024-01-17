2833733f4066d6271e4c8a8601fe4e62e1421c7d
w1zard913 <dobrinpaul2103@gmail.com>
2024-01-17 20:39:09 +0300

    Добавьте файлы проекта.

---
+++ ex3-1/MatrixArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class MatrixArrray
+    {
+        public int[,] array;
+        public MatrixArrray(int rows, int columns, bool IsUserInput = false)
+        {
+            if (IsUserInput)
+            {
+                this.array = this.UserFill(rows, columns);
+            }
+            else
+            {
+                this.array = this.RandomFill(rows, columns);
+            }
+        }
+        public int[,] RandomFill(int rows, int columns)
+        {
+            Random random = new Random();
+            int[,] array = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i, j] = random.Next(1, 1001);
+                }
+            }
+            return array;
+        }
+
+        public int[,] UserFill(int rows, int columns)
+        {
+            int[,] array = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return array;
+        }
+
+        public void DisplayArray()
+        {
+            for (int i = 0; i < this.array.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.array.GetLength(1); j++)
+                {
+                    string spaces = new string(' ', 5 - $"{this.array[i, j]}".Length);
+                    Console.Write($"{spaces}{this.array[i, j]}");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
---
+++ ex3-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Arrays;
+
+class Program
+{
+    static void Main() 
+    {
+        /*LineArray array = new LineArray(10);
+        Console.WriteLine(array.Mean());
+        array.OnlyUnique();
+        array.DeleteElementsUpper100();
+        array.DisplayArray();*/
+
+        MatrixArrray array2 = new MatrixArrray(10, 10);
+        array2.DisplayArray();
+    }
+}
